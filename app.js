36ad20351b1e9dc27e94fa5e28e937295ecfac58
Saheed Ogunsolu <saheedogunsolu2016@gmail.com>
2021-03-27 23:06:52 +0100

    dark theme done

---
+++ app.js
@@ -0,0 +1,28 @@
+var toggle = document.getElementById("toggle");
+const sun = document.querySelector(".sun");
+const moon = document.querySelector(".moon");
+
+var storedTheme =
+  localStorage.getItem("theme") ||
+  (window.matchMedia("(prefers-color-scheme: dark)").matches
+    ? "dark"
+    : "light");
+if (storedTheme)
+  document.documentElement.setAttribute("data-theme", storedTheme);
+
+toggle.onclick = function () {
+  var currentTheme = document.documentElement.getAttribute("data-theme");
+  var targetTheme = "light";
+
+  if (currentTheme === "light") {
+    targetTheme = "dark";
+    moon.classList.add("d-none");
+    sun.classList.remove("d-none");
+  } else {
+    sun.classList.add("d-none");
+    moon.classList.remove("d-none");
+  }
+
+  document.documentElement.setAttribute("data-theme", targetTheme);
+  localStorage.setItem("theme", targetTheme);
+};
